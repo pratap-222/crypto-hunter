f18d3218f3e3ba389972af02038a8dc943d9ac54
{Your-name} <{your-email-id}>
2022-12-06 20:04:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { makeStyles } from "@material-ui/core";
 import './App.css';
+import Header from './components/Header';
+import {BrowserRouter, Route} from 'react-router-dom'
+import Homepage from './Pages/Homepage';
+import CoinPage from './Pages/CoinPage';
 
 function App() {
+
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: "#14161a",
+      color: "white",
+      minHeight: "100vh"
+    }    
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header/>
+        <Route path='/' component = {Homepage} exact/>
+        <Route path='/coins/:id' component={CoinPage} />
+      </div>
+    </BrowserRouter>
   );
 }
 
